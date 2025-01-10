74b0480d60dc0c8be10920d4deffb5f097e4cc09
swarui <stevewarui88@gmail.com>
2025-01-10 12:14:03 +0300

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(<>
     <hr className="p-2 mt-40 border-gray-400"/>
-     <h3 style={{fontFamily:'Poppins', fontSize:'12px'}} className="text-center p-3  text-white" >Copyright  © 2024</h3>  
+     <h3 style={{fontFamily:'Poppins', fontSize:'12px'}} className="text-center p-3  text-white" >Copyright  © 2025</h3>  
     </>)
 }
 export default Footer
