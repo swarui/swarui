80d2ec2e5fced09f383e8731387f7f106676dd5a
swarui <stevewarui88@gmail.com>
2025-03-13 23:16:17 +0300

    frontend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
   className="text-3xl sm:text-3xl md:text-5xl font-medium "
   style={{ fontFamily: 'Afacad', fontSize: '2.5rem' }}  
 >
-  Hey There, I'm Steve Warui 
+  Hey There, I'm Steve  <span  className="text-blue-600" >Warui</span>  
 </h1>
 
 
@@ -17,7 +17,7 @@ function Home() {
   style={{ fontFamily: 'Afacad', fontSize: '1.2rem', lineHeight: '1.5' }}
   className="opacity-75 mt-10"
 >
-  I am a passionate Front-End Software Engineer committed to mastering my craft and sharing knowledge with fellow developers. I have a strong desire to contribute and continuously learn in the ever-evolving world of development. I am a passionate newbie Front-End Software Engineer eager to innovate new interactive applications and always ready to learn. I pay close attention to every design that I make and ensure its functionality as desired. I love bringing ideas to life, turning concepts into fully realized projects.
+  A passionate Front-End Software Engineer committed to mastering my craft and sharing knowledge with fellow developers. I have a strong desire to contribute and continuously learn in the ever-evolving world of development. I am a passionate newbie Front-End Software Engineer eager to innovate new interactive applications and always ready to learn. I pay close attention to every design that I make and ensure its functionality as desired. I love bringing ideas to life, turning concepts into fully realized projects.
 </h2>
 
 
