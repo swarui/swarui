7f3063008870ea5d213c01c55cd9f6e9a70b6adb
swarui <stevewarui88@gmail.com>
2025-03-14 13:25:33 +0300

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Home from "../src/components/Home";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
           <Home />
           <Projects />
           <Skills />
+          <Contact/>
         </div>
     </>
   );
