6f4c2d5f688ee47deb44eadfb88c8597cf7d69a4
swarui <stevewarui88@gmail.com>
2025-03-14 13:31:23 +0300

    frontend

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,20 +1,18 @@
 function Projects() {
-    return (
-      <>
-        <div
-          style={{ fontFamily: "Afacad" }}
-          id="projects"
-          className="max-w-[780px] w-[90%] mx-auto py-16"
-        >
-          <h1 className="text-left text-4xl font-bold text-white mb-2">
-            Reach Out
-          </h1>
-          <p className="text-left text-gray-400 mb-12">
-Connect with me :)          </p>
-        </div>
-      </>
-    );
-  }
-  
-  export default Projects;
-  
+  return (
+    <>
+      <div
+        style={{ fontFamily: "Afacad" }}
+        id="projects"
+        className="max-w-[780px] w-[90%] mx-auto py-16"
+      >
+        <h1 className="text-left text-4xl font-bold text-white mb-2">
+          Reach Out
+        </h1>
+        <p className="text-left text-gray-400 mb-12">Connect with me :) </p>
+      </div>
+    </>
+  );
+}
+
+export default Projects;
