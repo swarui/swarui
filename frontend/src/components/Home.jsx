5d9b3b56a889381706fce9a6975d5fbb01adccd9
swarui <stevewarui88@gmail.com>
2025-03-24 15:00:23 +0300

    Home Page Revamp

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,21 +7,29 @@ function Home() {
             Hey There, I'm Steve <span className="text-blue-600">Warui</span>
           </h1>
           <h2 className="opacity-75 mt-4 sm:mt-6 md:mt-10 text-md sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] leading-relaxed">
-            A passionate Front-End Software Engineer committed to mastering my craft and sharing knowledge with fellow
-            developers. I have a strong desire to contribute and continuously learn in the ever-evolving world of
-            development. I am a passionate newbie Front-End Software Engineer eager to innovate new interactive
-            applications and always ready to learn. I pay close attention to every design that I make and ensure its
-            functionality as desired. I love bringing ideas to life, turning concepts into fully realized projects.
+            A passionate Front-End Software Engineer, Architect, and UI/UX
+            Designer from Kenya, committed to mastering my craft and sharing
+            knowledge with fellow developers. I have a strong desire to
+            contribute and continuously learn in the ever-evolving world of
+            development. I pay close attention to every design I create,
+            ensuring both its aesthetic appeal and functionality. I thrive on
+            bringing ideas to life, transforming concepts into fully realized
+            projects and and finding creative solutions to challenging problems
           </h2>
-          <button className="cursor-pointer flex justify-between px-5 sm:px-6 py-2 text-white text-base sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] shadow-lg border border-blue-600 rounded-md mt-4 sm:mt-6 transition-all duration-300 hover:bg-blue-600/10 hover:scale-105 hover:shadow-blue-500/30 relative overflow-hidden group">
-            <span className="relative z-10">Resume</span>
-            <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
-          </button>
+          <a
+            href="https://drive.google.com/file/d/14NKDR7rKWh7AYZVydE-f7V18-uSysHZn/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button className="cursor-pointer flex justify-between px-5 sm:px-6 py-2 text-white text-base sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] shadow-lg border border-blue-600 rounded-md mt-4 sm:mt-6 transition-all duration-300 hover:bg-blue-600/10 hover:scale-105 hover:shadow-blue-500/30 relative overflow-hidden group">
+              <span className="relative z-10">Resume</span>
+              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+            </button>
+          </a>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Home
-
+export default Home;
