98d742556ce458eb42b9c5e80430b29617e84e28
swarui <stevewarui88@gmail.com>
2025-04-07 19:06:43 +0300

    swarui

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -408,20 +408,7 @@ function Contact() {
                         </>
                       ) : (
                         <>
-                          <svg
-                            className="w-3.5 h-3.5 sm:w-4 sm:h-4 mr-1.5 sm:mr-2"
-                            fill="none"
-                            stroke="currentColor"
-                            viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              strokeWidth={2}
-                              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                            />
-                          </svg>
+                        
                           Send Message
                         </>
                       )}
