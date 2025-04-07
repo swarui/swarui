c70e0a90eed09de44d033eb39a02daf8f52769a4
swarui <stevewarui88@gmail.com>
2025-04-07 19:25:24 +0300

    swarui

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -19,12 +19,13 @@ function Home() {
           <a
             href="https://drive.google.com/file/d/14NKDR7rKWh7AYZVydE-f7V18-uSysHZn/view?usp=sharing"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="cursor-pointer flex justify-between px-5 sm:px-6 py-2 text-white text-base sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] shadow-lg border border-blue-600 rounded-md mt-4 sm:mt-6 transition-all duration-300 hover:bg-blue-600/10 hover:scale-105 hover:shadow-blue-500/30 relative overflow-hidden group">
+            rel="noopener noreferrer">
+            <button className="cursor-pointer flex justify-between items-center px-5 sm:px-6 py-2 pb-3 text-white text-base sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] shadow-lg border border-blue-600 rounded-md mt-4 sm:mt-6 transition-all duration-300 hover:bg-blue-600/10  hover:shadow-blue-500/30 relative overflow-hidden group">
               <span className="relative z-10">Resume</span>
-              <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+              <span className="absolute bottom-1 left-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
             </button>
+            
+            
           </a>
         </div>
       </div>
