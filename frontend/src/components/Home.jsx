e65b9202691c9aaeac60fc2ae0c2acb5a7b6e397
swarui <stevewarui88@gmail.com>
2025-04-11 11:58:12 +0300

    swarui

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 function Home() {
   // Ensure the page starts at the Home section
@@ -7,7 +7,9 @@ function Home() {
   }, []); // Empty dependency array means this effect runs once on mount
   return (
     <>
-      <div id="home"  className="flex justify-center items-center  relative z-[1] pt-[5rem] sm:pt-[8rem] mt-[10vh] overflow-hidden">
+      <div
+        id="home"
+        className="flex justify-center items-center  relative z-[1] pt-[5rem] sm:pt-[8rem] mt-[10vh] overflow-hidden">
         <div className="max-w-[790px] w-[90%] mx-auto text-left px-2">
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[2.75rem] font-medium font-['Afacad']">
             Hey There, I'm Steve <span className="text-blue-600">Warui</span>
@@ -26,12 +28,12 @@ function Home() {
             href="https://drive.google.com/file/d/14NKDR7rKWh7AYZVydE-f7V18-uSysHZn/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer">
-            <button className="cursor-pointer flex justify-between items-center px-5 sm:px-6 py-2 pb-3 text-white text-base sm:text-lg md:text-xl lg:text-[1.15rem] font-['Afacad'] shadow-lg border border-blue-600 rounded-md mt-4 sm:mt-6 transition-all duration-300 hover:bg-blue-600/10  hover:shadow-blue-500/30 relative overflow-hidden group">
-              <span className="relative z-10">My Resume</span>
-              <span className="absolute bottom-1 left-0 w-0 h-[2px] bg-blue-500 transition-all duration-300 group-hover:w-full"></span>
+            <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-5 sm:px-6 lg:py-2 py-2 sm:py-3 text-sm sm:text-base md:text-lg lg:text-[1.15rem] font-['Afacad']  text-white transition-all duration-300 ease-in-out hover:shadow-xl hover:shadow-gray-600/50 border border-white/20 mt-4 sm:mt-6">
+              <span className="z-10">My Resume</span>
+              <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+                <div className="relative h-full w-10 bg-white/20"></div>
+              </div>
             </button>
-            
-            
           </a>
         </div>
       </div>
